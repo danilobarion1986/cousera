b78f6618a6bdcb1096cd54bca64fc1461f4a1a94
Danilo Barion Nogueira <danilo.barion@vagas.com.br>
2017-08-08 09:26:41 -0300

    WIP - Ajuste do .gitignore do package dos arquivos java.

--- algorithms-part-I/week-1/PercolationStats.java
+++ algorithms-part-I/week-1/PercolationStats.java
@@ -18,6 +18,7 @@ package edu.princeton.cs.algs4;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.Percolation;
 
  /*****************************************************************************
   * API:
